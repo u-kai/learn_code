ff3e5dd84cbbbb8b4a24e12d7d3d572e35cef545
u-kai <76635578+u-kai@users.noreply.github.com>
2021-12-13 19:48:52 +0900

    add standard knowladge

---
+++ learn-programing-by-js/src/classes/IntervalDisplayer.ts
@@ -0,0 +1,50 @@
+export class CilculationDisplayer {
+    private displayElements: HTMLElement[]
+    private displayIndex: number
+    constructor(displayElements: HTMLElement[]) {
+        this.displayElements = displayElements
+        this.displayIndex = 0
+    }
+    next = () => {
+        if (this.displayIndex === this.displayElements.length - 1) {
+            this.displayIndex = 0
+            return
+        }
+        this.displayIndex += 1
+        return
+    }
+    display = () => {
+        const numberOfElements = this.displayElements.length
+        for (let i = 0; i < numberOfElements; i++) {
+            if (this.displayIndex === i) {
+                this.displayElements[i].hidden = false
+            }
+            this.displayElements[i].hidden = true
+        }
+    }
+}
+
+export class IntervalFn {
+    private isRun: boolean
+    private intervalTime: number
+    private callback: () => void
+    private interval: NodeJS.Timeout
+    constructor(intervalTime: number, callback: () => void) {
+        this.isRun = false
+        this.intervalTime = intervalTime
+        this.callback = callback
+        this.interval = setInterval(() => {
+            this.callback()
+        }, this.intervalTime)
+    }
+    start = () => {
+        this.isRun = true
+        this.interval
+    }
+    stop = () => {
+        if (this.isRun === false) {
+            throw new Error('not start')
+        }
+        clearInterval(this.interval)
+    }
+}
--- learn-programing-by-js/src/index.ts
+++ learn-programing-by-js/src/index.ts
@@ -1,5 +1,7 @@
-import { InputWathcer } from './classes/InputWatcher'
-import { JANKEN } from './classes/JANKEN'
+//import { InputWathcer } from './classes/InputWatcher'
+//import { JANKEN } from './classes/JANKEN'
+
+import { IntervalFn } from './classes/IntervalDisplayer'
 
 //const atachEvent = () => {
 //const input = document.getElementById('count')
@@ -10,72 +12,91 @@ import { JANKEN } from './classes/JANKEN'
 //}
 //})
 //}
-const getNumberOfDuel = (): number | Error => {
-    const input = document.getElementById('count')
-    if (input instanceof HTMLInputElement) {
-        const { value } = input
-        if (/[1-9]+$/.test(value)) {
-            const numberOfDuel = Number(value)
-            return numberOfDuel
-        }
-    }
-    return new Error('数値を入れてね')
-}
-class CpJ {
-    private iter: HTMLElement[]
-    private nowIndex: number
-    constructor() {
-        const ch = document.getElementById('cpCh')
-        const gu = document.getElementById('cpGu')
-        const pa = document.getElementById('cpPa')
-        if (ch === null || gu === null || pa === null) {
-            throw new Error('error')
-        }
-        this.iter = [ch, gu, pa]
-        this.nowIndex = 0
-    }
-    displayNext = (): void => {
-        this.changeIndex()
-        this.iter.map((i) => {
-            i.hidden = true
-        })
-        console.log(this.nowIndex)
-        this.iter[this.nowIndex].hidden = false
-    }
-    changeIndex = (): void => {
-        if (this.nowIndex === this.iter.length - 1) {
-            console.log('odjfal')
-            this.nowIndex = 0
-            return
-        }
-        console.log('odjfal')
-        this.nowIndex = this.nowIndex + 1
-        return
-    }
-}
-const rundom = () => {
-    const cp = new CpJ()
-    setInterval(() => {
-        console.log('jodp')
-        cp.displayNext()
-    }, 200)
-}
-const button = document.getElementById('startBtn')
-if (button) {
-    const errorChildren = document.createElement('div')
-    errorChildren.textContent = '数値を入れてね'
-    errorChildren.id = 'errorMessage'
-    errorChildren.style.color = 'red'
-    const inputElement = document.getElementById('count')
-    const parentElement = document.getElementById('inputContainer')
-    if (!(inputElement instanceof HTMLInputElement)) {
-        throw new Error('not input')
-    }
-    if (parentElement && inputElement) {
-        const wathcer = new InputWathcer(inputElement, parentElement, errorChildren)
-        wathcer.setInputEvent()
+//const getNumberOfDuel = (): number | Error => {
+//const input = document.getElementById('count')
+//if (input instanceof HTMLInputElement) {
+//const { value } = input
+//if (/[1-9]+$/.test(value)) {
+//const numberOfDuel = Number(value)
+//return numberOfDuel
+//}
+//}
+//return new Error('数値を入れてね')
+//}
+//class CpJ {
+//private iter: HTMLElement[]
+//private nowIndex: number
+//constructor() {
+//const ch = document.getElementById('cpCh')
+//const gu = document.getElementById('cpGu')
+//const pa = document.getElementById('cpPa')
+//if (ch === null || gu === null || pa === null) {
+//throw new Error('error')
+//}
+//this.iter = [ch, gu, pa]
+//this.nowIndex = 0
+//}
+//displayNext = (): void => {
+//this.changeIndex()
+//this.iter.map((i) => {
+//i.hidden = true
+//})
+//console.log(this.nowIndex)
+//this.iter[this.nowIndex].hidden = false
+//}
+//changeIndex = (): void => {
+//if (this.nowIndex === this.iter.length - 1) {
+//this.nowIndex = 0
+//return
+//}
+//this.nowIndex = this.nowIndex + 1
+//return
+//}
+//}
+//const rundom = () => {
+//const cp = new CpJ()
+//setInterval(() => {
+//cp.displayNext()
+//}, 200)
+//}
+//const button = document.getElementById('startBtn')
+//if (button) {
+//const errorChildren = document.createElement('div')
+//errorChildren.textContent = '数値を入れてね'
+//errorChildren.id = 'errorMessage'
+//errorChildren.style.color = 'red'
+//const inputElement = document.getElementById('count')
+//const parentElement = document.getElementById('inputContainer')
+//if (!(inputElement instanceof HTMLInputElement)) {
+//throw new Error('not input')
+//}
+//if (parentElement && inputElement) {
+//const wathcer = new InputWathcer(inputElement, parentElement, errorChildren)
+//wathcer.setInputEvent()
 
-        button.addEventListener('click', () => console.log(wathcer.getNumberValue()))
-    }
-    rundom()
+//button.addEventListener('click', () => console.log(wathcer.getNumberValue()))
+//}
+//rundom()
+
+const infn = new IntervalFn(200, () => console.log('hello'))
+const start = new Promise((r) => {
+    console.log('fa')
+    infn.start()
+    return r
+})
+const stop = new Promise((r) => {
+    console.log('da')
+    infn.stop()
+    return r
+})
+const wait = new Promise((res, rej) => {
+    setTimeout(() => {
+        console.log('timeout')
+    }, 1000)
+    return res
+})
+;async () => {
+    await start
+    await wait
+    await stop
 }
